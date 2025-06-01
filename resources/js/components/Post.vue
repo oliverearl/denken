0bd30b04c8ee6243f86a7ae35a36304b36d06c34
Oliver Earl <oliver@oliverearl.co.uk>
2025-06-01 22:11:36 +0100

    Replace SVG icons with component-based icons in Post.vue

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import PostComment from '@/components/PostComment.vue';
+import CommentIcon from '@/components/ui/icon/CommentIcon.vue';
+import ShareIcon from '@/components/ui/icon/ShareIcon.vue';
+import HeartIcon from '@/components/ui/icon/HeartIcon.vue';
 
 interface User {
   name: string;
@@ -96,21 +99,15 @@ const sampleComments = ref<Comment[]>([
     <!-- Post Actions -->
     <div class="flex justify-between text-gray-500 dark:text-gray-400">
       <button @click="showComments = !showComments" class="flex items-center hover:text-blue-500">
-        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
-        </svg>
+        <CommentIcon class="mr-1" />
         {{ post.comments }}
       </button>
       <button class="flex items-center hover:text-green-500">
-        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
-        </svg>
+        <ShareIcon class="mr-1" />
         {{ post.shares }}
       </button>
       <button class="flex items-center hover:text-red-500">
-        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-        </svg>
+        <HeartIcon class="mr-1" />
         {{ post.likes }}
       </button>
     </div>
