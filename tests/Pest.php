21bfba4dd769903318fdef076c794627cb5e676a
Oliver Earl <oliver@oliverearl.co.uk>
2025-06-02 21:00:44 +0100

    Add integration test suite to phpunit configuration and update Pest extension

--- tests/Pest.php
+++ tests/Pest.php
@@ -14,8 +14,7 @@ declare(strict_types=1);
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\LazilyRefreshDatabase::class)
-    ->in('Feature');
+    ->in('Feature', 'Integration');
 
 /*
 |--------------------------------------------------------------------------
