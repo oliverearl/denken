9c416dc8737b5050df00bf0b89f4d867844a8377
Oliver Earl <oliver@oliverearl.co.uk>
2025-06-04 23:03:14 +0100

    Add ArchitectureTest to enforce coding standards and structure for application components

---
+++ tests/Feature/ArchitectureTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Mail\Mailable;
+
+arch()->preset()->php();
+arch()->preset()->laravel();
+arch()->preset()->security();
+
+arch()
+    ->expect('App')
+    ->toHaveMethodsDocumented()
+    ->toHavePropertiesDocumented()
+    ->toUseStrictEquality()
+    ->toUseStrictTypes();
+
+arch()
+    ->expect('App\Console\Client')
+    ->toBeClasses()
+    ->toBeReadonly()
+    ->toBeInvokable();
+
+arch()
+    ->expect('App\Events')
+    ->classes()
+    ->toBeReadonly()
+    ->toHaveSuffix('Event');
+
+arch()
+    ->expect('App\Jobs\Checks')
+    ->toHaveSuffix('Check');
+
+arch()
+    ->expect('App\Listeners')
+    ->classes()
+    ->toHaveSuffix('Listener');
+
+arch()
+    ->expect('App\Mail')
+    ->classes()
+    ->toHaveSuffix('Mail')
+    ->toExtend(Mailable::class);
+
+arch()
+    ->expect('App\Models')
+    ->toBeClasses()
+    ->toUseTrait(HasFactory::class);
+
+arch()
+    ->expect('App\Objects')
+    ->toBeClasses()
+    ->toBeReadonly()
+    ->toImplement(Arrayable::class)
+    ->toImplement(JsonSerializable::class);
+
+arch()
+    ->expect('App\Repositories')
+    ->toBeClasses()
+    ->toBeReadonly()
+    ->toHaveSuffix('Repository');
+
+arch()
+    ->expect('App\Services')
+    ->toBeClasses()
+    ->toHaveSuffix('Service');
